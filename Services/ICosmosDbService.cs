5457521560eff875cf9845e9db9b834f9b2951d2
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-09-23 15:42:53 +0200

    S

---
+++ Services/ICosmosDbService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace blog6WebApp
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<User>> GetUsersAsync(string query);
+        Task<User> GetUserAsync(string id);
+        Task AddUserAsync(User item);
+        Task UpdateUserAsync(string id, User item);
+        Task DeleteUserAsync(string id);
+    }
+}
